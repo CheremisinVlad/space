aab81363e027fd5b3e4cc2c230c85256f18edf9b
cheremisinVlad <cheremisinvladw@gmail.com>
2019-10-03 18:24:06 +0300

    registration form impl

---
+++ front/src/services/registration/__mocks__/index.js
@@ -0,0 +1,9 @@
+export default {
+  register(detail){
+    return new Promise((resolve, reject) => {
+      detail.email ==="vlad@mail.com"
+      ?resolve({result: 'success'})
+       :reject(new Error('user already exist'))
+    })
+  }
+}
--- front/tests/unit/RegisterPage.spec.js
+++ front/tests/unit/RegisterPage.spec.js
@@ -1,17 +1,93 @@
-import Vue from 'vue'
+import { mount, createLocalVue } from '@vue/test-utils'
+import VueRouter from 'vue-router'
 import RegisterPage from '@/views/RegisterPage'
 
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
+
+jest.mock('@/services/registration')
+
 describe("RegisterPage.vue",()=>{
-    it('should render correct contents',()=>{
-      const Constructor = Vue.extend(RegisterPage)
-      const vm = new Constructor().$mount()
-      expect(vm.$el.querySelector('.logo').getAttribute('src'))
-        .toEqual('/images/logo.png')
-      expect(vm.$el.querySelector('.tagline').textContent)
-        .toEqual('Organize your space like your want')
-      expect(vm.$el.querySelector('#username').value).toEqual('')
-      expect(vm.$el.querySelector('#email').value).toEqual('')
-      expect(vm.$el.querySelector('#password').value).toEqual('')
-      expect(vm.$el.querySelector('form button[type="submit"]').textContent).toEqual('Create account')
+    let wrapper
+    let userNameField
+    let emailField
+    let passwordField
+    let buttonSubmit
+    beforeEach(()=>{
+      wrapper = mount(RegisterPage,{
+        localVue,router
+      })
+      userNameField = wrapper.find('#username')
+      emailField = wrapper.find('#email')
+      passwordField = wrapper.find('#password')
+      buttonSubmit = wrapper.find('form button[type="submit"]')
     })
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('should render registration form',()=>{
+    expect(wrapper.find('.logo').attributes().src).toEqual('/images/logo.png')
+    expect(wrapper.find('.tagline').text()).toEqual('Organize your space like your want')
+    expect(userNameField.element.value).toEqual('')
+    expect(emailField.element.value).toEqual('')
+    expect(passwordField.element.value).toEqual('')
+    expect(buttonSubmit.text()).toEqual('Create account')
+  })
+
+  it('should contain data with empty initial values',()=>{
+    expect(wrapper.vm.form.username).toEqual('')
+    expect(wrapper.vm.form.email).toEqual('')
+    expect(wrapper.vm.form.password).toEqual('')
+  })
+
+  it('should have form inputs bound with data',()=>{
+    const username = 'vlad'
+    const email = 'vlad@mail.com'
+    const password = 'vlad'
+
+    wrapper.vm.form.username = username
+    wrapper.vm.form.email = email
+    wrapper.vm.form.password = password
+
+    expect(userNameField.element.value).toEqual(username)
+    expect(emailField.element.value).toEqual(email)
+    expect(passwordField.element.value).toEqual(password)
+  })
+
+  it('should have form submit handler `submitForm`',()=>{
+    const stub = jest.fn()
+    wrapper.setMethods({submitForm: stub})
+    buttonSubmit.trigger('submit')
+    expect(stub).toBeCalled()
+  })
+
+  it('should register when new user come',()=>{
+    const stub = jest.fn()
+    wrapper.vm.$router.push = stub
+    wrapper.vm.form.username = 'vlad'
+    wrapper.vm.form.email = 'vlad@mail.com'
+    wrapper.vm.form.password = 'vlad'
+    wrapper.vm.submitForm()
+    wrapper.vm.$nextTick(()=>{
+      expect(stub).toHaveBeenCalledWith({name: 'LoginPage'})
+    })
+  })
+
+  it('should fail with user is not new ',()=>{
+    
+    wrapper.vm.form.email = 'vasya@mail.com'
+    expect(wrapper.find('.failed').isVisible()).toBe(false)
+    wrapper.vm.submitForm()
+    wrapper.vm.$nextTick(()=>{
+      expect(wrapper.find('.failed').isVisible()).toBe(true)
+    })
+  })
+
+
+
+
+
+
 })
