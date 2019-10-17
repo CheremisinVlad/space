420af6c14cfbcbef963ffc4ad82e551ed6052c38
cheremisinVlad <cheremisinvladw@gmail.com>
2019-10-17 18:46:00 +0300

    Home page and Header impl

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,30 +1,73 @@
 <template>
-  <div class="header-nav">
+  <div class="page-header d-flex bd-highlight">
     <div class="logo">
-
+      <img class="logo" src="/images/logo.png">
     </div>
-    <div class="directories-toggle">
-
+    <div class="search-box flex-fill" >
+      <div class="search-wrapper">
+        <input type="text" placeholder="Search" class="form-control form-control-sm" />
+      </div>
     </div>
-    <div class="urls-toggle">
-
+    <div class="profile-toggle">
+      <div class="dropdown">
+        <button class="btn dropdown-toggle" type="button" id="profileMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Vlad
+        </button>
+        <div class="dropdown-menu" aria-labelledby="profileMenu">
+          <button class="dropdown-item" type="button">Profile</button>
+          <button class="dropdown-item" type="button">Sign Out</button>
+        </div>
+      </div>
     </div>
-    <div class="urls-toggle">
-
-    </div>
-
-
   </div>
 
 </template>
-
 <script>
+
     export default {
         name: 'Header'
     }
 </script>
 
 <style lang="scss" scoped>
+  .page-header {
+    padding: 12px 12px 12px;
+    border-bottom: 1px solid #f4e9dc;
+  }
+  .logo {
+    color: #444;
+    height: 60px;
+    width: 120px;
+    margin-top: 2px;
+  }
+  img {
+    margin-left: 5px;
+    width: 80px;
+    height: 20px;
+    vertical-align: bottom;
+  }
+
+  .profile-toggle {
+    width: 120px;
+    text-align: right;
+  }
+  .dropdown-toggle {
+    padding: 2px 5px !important;
+  }
+  .search-wrapper {
+    width: 150px;
+    margin: 0 auto;
+    position: relative;
+  }
+  input {
+    padding-left: 30px;
+    height: calc(1.8125rem + 5px);
+    font-size: 1rem;
+    border: 1px solid #eee;
+  }
+  input:focus {
+    border: 1px solid #377EF6;
+  }
 
 
 </style>
--- front/src/views/HomePage.vue
+++ front/src/views/HomePage.vue
@@ -1,13 +1,49 @@
 <template>
-  <div><h1 class="page-title">Home</h1></div>
+  <div>
+    <Header/>
+      <div class="space-container">
+        <div class="spaces d-flex align-content-start flex-wrap">
+          <div class="space list-inline-item">
+            <div class="space-wrapper">Patterns</div>
+          </div>
+          <div class="space list-inline-item">
+            <div class="space-wrapper">Urls</div>
+          </div>
+          <div class="create-space-wrapper">
+            <button class="btn btn-link">+ Create New Space</button>
+          </div>
+        </div>
+      </div>
+
+  </div>
 </template>
 
 <script>
+  import Header from '@/components/Header.vue'
     export default {
-        name: "HomePage"
+        name: "HomePage",
+        components: {
+          Header
+        }
     }
 </script>
 
 <style scoped>
+  .space-container {
+    padding: 0 35px;
+  }
+  .space-wrapper {
+    position: center;
+  }
+  .space {
+    width: 270px;
+    height: 110px;
+    border-radius: 5px;
+    background-color: #377EF6;
+    color: #fff;
+    padding: 15px;
+    margin-right: 100px;
+    cursor: pointer;
+  }
 
 </style>
